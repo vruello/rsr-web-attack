565f34e6a81531278eb7bef16b9e04ced681655b
Vincent Ruello <hello@vruello.fr>
2019-01-22 13:56:28 +0100

    Update phantomjs.js

--- scenario1/extern/phantomjs.js
+++ scenario1/extern/phantomjs.js
@@ -1,19 +1,16 @@
+function loginAndBrowse() {
 
-phantom.clearCookies();
+        phantom.clearCookies();
 
-// login
-var page = require('webpage').create();
+        var page = require('webpage').create();
+          
+        page.open('http://localhost/?p=login', 'POST', 'username=salmon&password=cGWhbAUmTe4JGf4J', function(status) {
+                console.log("http://localhost/?p=login. Status: " + status);
+                page.open('http://localhost/',  function(status) {
+                        console.log("http://localhost/. Status: " + status);
+                });
+        });
 
-page.open('http://localhost/scenario1/public/?p=login', 'POST', 'username=salmon&password=cGWhbAUmTe4JGf4J', function(status) {
-  console.log("http://localhost/scenario1/public/?p=login. Status: " + status);
-});
-
-
-function load() {
-  page.open('http://localhost/scenario1/public/',  function(status) {
-    console.log("http://localhost/scenario1/public/. Status: " + status);
-  });
 }
 
-setInterval(load, 5000);
-
+setInterval(loginAndBrowse, 5000);
